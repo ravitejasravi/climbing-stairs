91a0a6afcdbd73fa7e8623f39acaea9ede9e1518
Raviteja S <43201927+ravitejasravi@users.noreply.github.com>
2023-01-14 22:28:35 +0530

    Solution

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int f3 = 0, f1 = 1, f2 = 2;
+        
+        if(n <= 2)
+            return n; 
+        
+        for(int i = 3; i <= n; i++){
+            
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+            
+        }
+        
+        return f3;
+    }
+};
